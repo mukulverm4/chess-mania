b0d2eff091c6833702c3e8b3dd073512e543d596
HotFudge <vermamukul44@gmail.com>
2019-04-19 23:57:09 +0530

    Add move making

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View,Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View,Dimensions, TouchableOpacity ,Button} from 'react-native';
 import Chess from 'chess.js';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const ROWS = 8;
+const ALPHABETS = ['a','b','c','d','e','f','g','h'];
 
 
 
@@ -12,32 +13,75 @@ export default class App extends React.Component {
     super();
     let chess = Chess.Chess;
     let chessMania = new chess();
-    console.log(chessMania.ascii());
-    console.log(chessMania.fen());
-    console.log(chessMania.pgn());
+    let currentGameFEN = chessMania.fen();
     // while (!chessMania.game_over()) {
     //   var moves = chessMania.moves();
     //   var move = moves[Math.floor(Math.random() * moves.length)];
     //   chessMania.move(move);
     // }
     // console.log(chessMania.pgn());
-
-    var {height, width} = Dimensions.get('window');
     this.state = {
-      width:width,
+      currentGameFEN : currentGameFEN,
+      from:'',
+      to:'',
+      currentGame:chessMania,
     }
+
+  }
+
+  _getFenArray(){
+
+    let fen = this.state.currentGameFEN;
+    console.log(fen)
+    let fenArray = (fen.split(" ")[0]).split("/");
+    let fenRowArray = [];
+
+    for(let i = 0; i < fenArray.length; i++){
+      //
+      let tempArray = fenArray[i].split("");
+      if(tempArray.length === ROWS)
+        fenRowArray[i] = tempArray;
+      else{
+        tempRow = []
+        let tempRowIndex = 0;
+        for(let j = 0; j < tempArray.length; j++){
+          console.log(Number(tempArray[j]));
+          console.log("#########", tempArray[j])
+          if(!isNaN(tempArray[j])){
+            
+              let count = 0;
+              while(count  < tempArray[j]){
+                tempRow[tempRowIndex] = 'o';
+                tempRowIndex++;
+                count++;
+              }
+            }
+          else{
+            console.log("######",tempRow)
+            tempRow[tempRowIndex] = tempArray[j];
+            tempRowIndex++;
+          }
+          }
+          fenRowArray[i] = tempRow;
+        }
+      }
+      
+      return fenRowArray;
   }
 
   _renderBoard(){
 
     const rows = [];
 
+    let fenRowArray = this._getFenArray();
+    //console.log(fenRowArray)
     for (let i = 0; i < ROWS; i++){
       const cells = [];
       for (let j = 0; j < ROWS; j++){
         cells.push(
-        <TouchableOpacity onPress={()=>this._onTouch(""+i+j)} key={""+i+j} style={styles.cell}>
-          <Icon name='chess-pawn' size={32}/>
+        <TouchableOpacity onPress={()=>this._onTouch(ALPHABETS[j]+(i+1))} key={ALPHABETS[i]+(j+1)} style={styles.cell}>
+          {fenRowArray[i][j]==="o"||fenRowArray[i][j]=="1"?null:<Icon name={this._getPiece(fenRowArray[i][j])} size={32}
+           color={fenRowArray[i][j] == (fenRowArray[i][j]).toLowerCase()  ? "#f00" : "#0f0" }/>}
         </TouchableOpacity>)
       }
       rows.push(<View key={i} style={[styles.row]}>{cells}</View>)
@@ -50,10 +94,16 @@ export default class App extends React.Component {
   }
 
   _onTouch(key){
-    console.log("##########=> ", key)
+    if(this.state.from === ''){
+      this.setState({from:key})
+    }
+    else{
+      this.setState({to:key})
+    }
   }
 
   _getPiece(letter){
+   
     if (letter.toUpperCase() ==='R')
       return 'chess-rook';
     else if(letter.toUpperCase() === 'B')
@@ -64,7 +114,32 @@ export default class App extends React.Component {
       return 'chess-queen';
     else if(letter.toUpperCase() === 'K')
       return 'chess-king';
-    else throw new Error('Piece not found!');
+    else if(letter.toUpperCase() === 'P')
+      return 'chess-pawn';
+    else return '' ;
+     //throw new Error('Piece not found!');console.log("##########=> ", key)
+  }
+
+  _makeMove(){
+    if(this.state.from !== '' && this.state.to !== ''){
+      console.log(" made move => ", this.state.from," to ", this.state.to);
+      let chess = Chess.Chess;
+      let chessMania = new chess(this.state.currentGameFEN);
+      let thisMove = {from:this.state.from, to:this.state.to};
+      console.log(thisMove)
+      let move = chessMania.move(thisMove);
+      console.log("move => ",move)
+      let currGame = chessMania.fen();
+      console.log(currGame);
+
+      this.setState({
+        from : '',
+        to : '',
+        currentGameFEN : currGame
+      })
+
+    }
+  
   }
 
 
@@ -72,9 +147,9 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
       <View style={styles.chessBoard}>
-  
-              {this._renderBoard()}
+          {this._renderBoard()}
               </View>
+        <Button title="Make move" onPress={()=>this._makeMove()}></Button>
       </View>
     );
   }
