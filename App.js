b40a72b46cb9ae0bc95450a88103d717cfecf60e
HotFudge <vermamukul44@gmail.com>
2019-04-23 15:20:00 +0530

    Integrate firebase

--- App.js
+++ App.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions, TouchableOpacity, Button } from 'react-native';
 import ChessMania from './src/components/ChessMania';
-
+import firebase from 'firebase';
 
 export default class App extends React.Component {
   constructor() {
     super();
-  }
 
+
+    var config = {
+      apiKey: "AIzaSyDmuUvqvGvRtmTjijXAxOKH9kK6H3OHrPk",
+      authDomain: "chess-menace.firebaseapp.com",
+      databaseURL: "https://chess-menace.firebaseio.com",
+      projectId: "chess-menace",
+      storageBucket: "chess-menace.appspot.com",
+      messagingSenderId: "31372489105"
+    };
+    firebase.initializeApp(config);
+    
+    this.state = {
+      user : ''
+    }
+
+  }
+  componentDidMount(){
+    firebase.auth().signInAnonymously().catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      console.log("firebase error ", errorCode, errorMessage);
+    });
+    
+    firebase.auth().onAuthStateChanged((user)=>{
+      if (user) {
+        // User is signed in.
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        this.setState({user:user})
+        console.log(user);
+        // ...
+      } else {
+        // User is signed out.
+        // ...
+        console.log("not signed in")
+      }
+      // ...
+    });
+  }
 
   render() {
     return (
